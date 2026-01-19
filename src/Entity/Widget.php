f33852374013fafc60e8637dfd49eaeda82af8d2
EZ-ZARZOURI <houda.ezzarzouri@gmail.com>
2026-01-19 20:57:57 +0100

    Add login functionality, User and Widget entities and repositories

---
+++ src/Entity/Widget.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WidgetRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: WidgetRepository::class)]
+class Widget
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'widgets')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $owner = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $filterType = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $filterValue = null;
+
+    #[ORM\Column]
+    private ?int $position = null;
+
+    #[ORM\Column]
+    private ?int $page = null;
+
+    #[ORM\Column]
+    private ?int $pageSize = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getOwner(): ?User
+    {
+        return $this->owner;
+    }
+
+    public function setOwner(?User $owner): static
+    {
+        $this->owner = $owner;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getFilterType(): ?string
+    {
+        return $this->filterType;
+    }
+
+    public function setFilterType(?string $filterType): static
+    {
+        $this->filterType = $filterType;
+
+        return $this;
+    }
+
+    public function getFilterValue(): ?string
+    {
+        return $this->filterValue;
+    }
+
+    public function setFilterValue(?string $filterValue): static
+    {
+        $this->filterValue = $filterValue;
+
+        return $this;
+    }
+
+    public function getPosition(): ?int
+    {
+        return $this->position;
+    }
+
+    public function setPosition(int $position): static
+    {
+        $this->position = $position;
+
+        return $this;
+    }
+
+    public function getPage(): ?int
+    {
+        return $this->page;
+    }
+
+    public function setPage(int $page): static
+    {
+        $this->page = $page;
+
+        return $this;
+    }
+
+    public function getPageSize(): ?int
+    {
+        return $this->pageSize;
+    }
+
+    public function setPageSize(int $pageSize): static
+    {
+        $this->pageSize = $pageSize;
+
+        return $this;
+    }
+}
