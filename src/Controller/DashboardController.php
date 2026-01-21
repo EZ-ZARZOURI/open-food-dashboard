cbbf8838ff98b0a36cbff1f5466793c088418b77
EZ-ZARZOURI <houda.ezzarzouri@gmail.com>
2026-01-21 02:46:02 +0100

    feat(dashboard): add addWidget and editWidget

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -8,6 +8,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Widget;
+use Doctrine\ORM\EntityManagerInterface;
 
 class DashboardController extends AbstractController
 {
@@ -61,5 +63,72 @@ class DashboardController extends AbstractController
         return $this->render('dashboard/index.html.twig', [
             'widgetsWithProducts' => $widgetsWithProducts,
         ]);
+    }
+
+    #[Route('/dashboard/add-widget', name: 'dashboard_add_widget', methods: ['POST'])]
+    public function addWidget(
+        Request $request,
+        WidgetRepository $widgetRepo,
+        EntityManagerInterface $em
+    ): Response {
+        $user = $this->getUser();
+        if (!$user) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $filterType = $request->request->get('filterType');
+        $filterValue = $request->request->get('filterValue');
+
+        if ($filterType && $filterValue) {
+            $widget = new Widget();
+            $widget->setOwner($user)
+                ->setType('products')
+                ->setFilterType($filterType)
+                ->setFilterValue($filterValue);
+
+            // Calculer position automatique
+            $lastWidget = $widgetRepo->findOneBy(['owner' => $user], ['position' => 'DESC']);
+            $position = $lastWidget ? $lastWidget->getPosition() + 1 : 1;
+            $widget->setPosition($position);
+
+            $em->persist($widget);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('dashboard_home');
+    }
+
+    #[Route('/dashboard/edit-widget', name: 'dashboard_edit_widget', methods: ['POST'])]
+    public function editWidget(
+        Request $request,
+        WidgetRepository $widgetRepo,
+        EntityManagerInterface $em
+    ): Response {
+        $user = $this->getUser();
+        if (!$user) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $id = $request->request->get('id');
+        $filterType = $request->request->get('filterType');
+        $filterValue = $request->request->get('filterValue');
+
+        if (!$id || !$filterType || !$filterValue) {
+            return $this->redirectToRoute('dashboard_home');
+        }
+
+        $widget = $widgetRepo->find($id);
+
+        if (!$widget || $widget->getOwner() !== $user) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $widget
+            ->setFilterType($filterType)
+            ->setFilterValue($filterValue);
+
+        $em->flush();
+
+        return $this->redirectToRoute('dashboard_home');
     }
 }
