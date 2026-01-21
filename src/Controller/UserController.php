561dc8445de46f2ea28228912aa0dc9b5a4c1142
EZ-ZARZOURI <houda.ezzarzouri@gmail.com>
2026-01-21 16:26:42 +0100

    Update CRUD for User: modify pages Delete, Form, Edit, Index, and New

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,6 +10,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[Route('/user')]
 final class UserController extends AbstractController
@@ -17,6 +18,7 @@ final class UserController extends AbstractController
     #[Route(name: 'app_user_index', methods: ['GET'])]
     public function index(UserRepository $userRepository): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         return $this->render('user/index.html.twig', [
             'users' => $userRepository->findAll(),
         ]);
@@ -25,11 +27,23 @@ final class UserController extends AbstractController
     #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
         $user = new User();
-        $form = $this->createForm(UserType::class, $user);
+
+        // Formulaire avec mot de passe obligatoire
+        $form = $this->createForm(UserType::class, $user, ['is_new' => true]);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // Récupérer le rôle choisi
+            $role = $form->get('role')->getData();
+            $user->setRoles([$role]);
+
+            // Hash du mot de passe
+            $plainPassword = $form->get('password')->getData();
+            $user->setPassword(password_hash($plainPassword, PASSWORD_DEFAULT));
+
             $entityManager->persist($user);
             $entityManager->flush();
 
@@ -42,9 +56,13 @@ final class UserController extends AbstractController
         ]);
     }
 
+
+
+
     #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
     public function show(User $user): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         return $this->render('user/show.html.twig', [
             'user' => $user,
         ]);
@@ -53,10 +71,28 @@ final class UserController extends AbstractController
     #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
-        $form = $this->createForm(UserType::class, $user);
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
+
+        // Formulaire avec mot de passe facultatif
+        $form = $this->createForm(UserType::class, $user, ['is_new' => false]);
+
+        // Préremplir le rôle actuel
+        $currentRole = in_array('ROLE_ADMIN', $user->getRoles()) ? 'ROLE_ADMIN' : 'ROLE_USER';
+        $form->get('role')->setData($currentRole);
+
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // Mettre à jour le rôle
+            $role = $form->get('role')->getData();
+            $user->setRoles([$role]);
+
+            // Hash du mot de passe seulement si saisi
+            $plainPassword = $form->get('password')->getData();
+            if ($plainPassword) {
+                $user->setPassword(password_hash($plainPassword, PASSWORD_DEFAULT));
+            }
+
             $entityManager->flush();
 
             return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
@@ -71,6 +107,7 @@ final class UserController extends AbstractController
     #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
     public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($user);
             $entityManager->flush();
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -4,24 +4,44 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
-        ;
+            ->add('email', EmailType::class)
+            ->add('role', ChoiceType::class, [
+                'mapped' => false,
+                'choices' => [
+                    'Utilisateur' => 'ROLE_USER',
+                    'Administrateur' => 'ROLE_ADMIN',
+                ],
+                'expanded' => true,
+                'multiple' => false,
+                'label' => 'Rôle',
+            ])
+            ->add('password', PasswordType::class, [
+                'mapped' => false,
+                'required' => $options['is_new'], // obligatoire si ajout
+                'constraints' => $options['is_new'] ? [
+                    new Assert\NotBlank(message: 'Veuillez renseigner ce champ'),
+                ] : [],
+                'label' => 'Mot de passe',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => User::class,
+            'is_new' => true,
         ]);
     }
 }
